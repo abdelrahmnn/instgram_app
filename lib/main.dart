40541d403c26f55aaff2038e71b137981a943109
Your Name <you@example.com>
2023-04-27 05:06:36 -0700

    return values

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instgram_app/responsive/mobile.dart';
 import 'package:instgram_app/responsive/responsive.dart';
+import 'package:instgram_app/responsive/web.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Responsive(),
+      home: Responsive(
+        mymobilscreen: MobileScreen(),
+        mywebscreen: WebScreen(),
+      ),
     );
   }
 }
