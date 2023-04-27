1e3c973b1f5e4dc836cabe53d34cc08074b73532
Your Name <you@example.com>
2023-04-27 08:40:01 -0700

    do new widget named addprofile

--- lib/screen/add_poste.dart
+++ lib/screen/add_poste.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class AddProfile extends StatefulWidget {
+  const AddProfile({super.key});
+
+  @override
+  State<AddProfile> createState() => _AddProfileState();
+}
+
+class _AddProfileState extends State<AddProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
