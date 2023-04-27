28296ea36e838abc78e35060c24057158205e559
Your Name <you@example.com>
2023-04-27 04:06:08 -0700

    scaffold

---
+++ lib/responsive/web.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WebScree extends StatefulWidget {
+  const WebScree({super.key});
+
+  @override
+  State<WebScree> createState() => _WebScreeState();
+}
+
+class _WebScreeState extends State<WebScree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
