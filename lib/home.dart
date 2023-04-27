a260ef843a3f944b2e4394d399cd08172f3ac788
Your Name <you@example.com>
2023-04-27 03:31:28 -0700

    ew used the variable named widthScreen

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,11 +7,10 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double widthScreen = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: MediaQuery.of(context).size.width > 600
-            ? Text("mobil screen")
-            : Text("web screen"),
+        title: widthScreen > 600 ? Text("mobil screen") : Text("web screen"),
       ),
     );
   }
