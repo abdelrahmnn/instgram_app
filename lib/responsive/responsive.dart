5814d10924f4ebc2f7b7092b5d25f57d5a363727
Your Name <you@example.com>
2023-04-27 04:36:37 -0700

    layoutBuilder

--- lib/responsive/responsive.dart
+++ lib/responsive/responsive.dart
@@ -10,6 +10,6 @@ class Responsive extends StatefulWidget {
 class _ResponsiveState extends State<Responsive> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return LayoutBuilder(builder: ,);
   }
 }
