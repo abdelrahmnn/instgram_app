6938011df97a61aa02d1d79da6dd02d4d6a61510
Your Name <you@example.com>
2023-04-27 04:35:07 -0700

    class responsive

---
+++ lib/responsive/responsive.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Responsive extends StatefulWidget {
+  const Responsive({super.key});
+
+  @override
+  State<Responsive> createState() => _ResponsiveState();
+}
+
+class _ResponsiveState extends State<Responsive> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
